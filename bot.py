1175fe471ec9cf4942f5c1c4d550128b494e6090
Hernan Amiune <amiune@gmail.com>
2024-01-31 14:38:16 +0100

    fixed weaviate healthcheck

--- bot.py
+++ bot.py
@@ -61,31 +61,18 @@ question1_uuid = questions.data.insert(
                 "question": "Como esta el clima hoy?",
                 "answer": "Muy lindo",
             },
-            #vector=[-0.5,-0.5,-0.5]
             vector=embeddings.embed_query("Como esta el clima hoy?")
         )
-st.chat_message("assistant").write(embeddings.embed_query("Como esta el clima hoy?")[0:5])
 
 question2_uuid = questions.data.insert(
             properties={
                 "question": "De que color es el pasto?",
                 "answer": "verde",
             },
-            #vector=[0.5,0.5,0.5]
             vector=embeddings.embed_query("De que color es el pasto?")
         )
 
-response = questions.query.near_vector(
-    near_vector=embeddings.embed_query("De que color es el pasto?"),
-    #near_vector=[0.5,0.5,0.5], # your query vector goes here,
-    #certainty=1, # fime the same vector
-    limit=2
-)
 
-st.chat_message("assistant").write(response.objects[0].properties)
-st.chat_message("assistant").write(response.objects[1].properties)
-
-client.close()
 
 llm = ChatOllama(
         temperature=0,
@@ -100,6 +87,8 @@ llm = ChatOllama(
 
 template = """
 You are a helpful assistant that helps to answer human questions.
+You can only speak only in spanish!!
+Don't answer any question in other language different from Spanish!!!
 """
 system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 human_template = "{question}"
@@ -110,13 +99,30 @@ chat_prompt = ChatPromptTemplate.from_messages(
 
 chain = chat_prompt | llm
 
-# Streamlit chat prompt
-if prompt := st.chat_input(placeholder="Escribe aqui tu pregunta"):
+rag_enabled = "No"
+rag_enabled = st.radio("Usar RAG para contestar", ["No", "Si"], horizontal=True)
 
+# Streamlit chat prompt
+prompt = st.chat_input(placeholder="Escribe aqui tu pregunta")
+
+if prompt:
     # Show the user prompt in the chat
     st.chat_message("user").write(prompt)
     
     # Run the chain
-    response = chain.invoke({"question": prompt})
+    if rag_enabled == "No":
+        response = chain.invoke({"question": prompt})
+        st.chat_message("assistant").write(response.content)
+    else:
+        response = questions.query.near_vector(
+            near_vector=embeddings.embed_query(prompt),
+            limit=2
+        )
+        r1 = f"La pregunta mas parecida en la BD es {response.objects[0].properties}"
+        st.chat_message("assistant").write(r1)
+        r2 = f"La segunda pregunta mas parecida en la BD es {response.objects[1].properties}"
+        st.chat_message("assistant").write(r2)
     
-    st.chat_message("assistant").write(response)
+
+
+#client.close()
